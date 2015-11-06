ec34e4711e21e287f7b568f8032c0e224201656c
Xavier Pedrals <xavier.pedrals.camprubi@est.fib.upc.edu>
2015-11-06 21:56:50 +0100

    Google Maps funca

--- app/src/main/java/com/grupo20/finapps/fastrackt/MainActivity.java
+++ app/src/main/java/com/grupo20/finapps/fastrackt/MainActivity.java
@@ -1,5 +1,6 @@
 package com.grupo20.finapps.fastrackt;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +9,11 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button mapsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +21,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        mapsButton = (Button) findViewById(R.id.buttonMaps);
+        mapsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, MapsActivity.class));
+            }
+        });
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
