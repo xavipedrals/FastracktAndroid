9c01dfe58937476fb3b7e8c6f2b4859cd63abe77
Xavier Pedrals <xavier.pedrals.camprubi@est.fib.upc.edu>
2015-11-06 22:56:38 +0100

    Permisos no peten en Android6

--- app/src/main/java/com/grupo20/finapps/fastrackt/MapsActivity.java
+++ app/src/main/java/com/grupo20/finapps/fastrackt/MapsActivity.java
@@ -1,5 +1,8 @@
 package com.grupo20.finapps.fastrackt;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
@@ -8,11 +11,38 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private Marker myMarker;
+
+
+    private GoogleMap.OnMyLocationChangeListener myLocationChangeListener = new GoogleMap.OnMyLocationChangeListener() {
+        @Override
+        public void onMyLocationChange(Location location) {
+            LatLng loc = new LatLng(location.getLatitude(), location.getLongitude());
+            myMarker = mMap.addMarker(new MarkerOptions().position(loc));
+            if (myMarker != null){
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(loc, 16.0f));
+            }
+        }
+    };
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission granted!
+                } else {
+                    // permission denied!
+                }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,11 +66,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
+        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        } else {
+            mMap = googleMap;
+            mMap.setMyLocationEnabled(true);
+        }
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
+
+
+
+
 }
---
+++ app/src/main/java/com/grupo20/finapps/fastrackt/myLocationChangeListener.java
@@ -0,0 +1,7 @@
+package com.grupo20.finapps.fastrackt;
+
+/**
+ * Created by xavi on 06/11/15.
+ */
+public class myLocationChangeListener {
+}
