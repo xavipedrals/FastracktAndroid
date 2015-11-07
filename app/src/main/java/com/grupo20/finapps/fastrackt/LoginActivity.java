8708727ffbb5ecaed2c17e3b4dcb5e5a3bb4a734
Albert Segarra Roca <albertsgrc@gmail.com>
2015-11-07 06:44:28 +0100

    Added post request

--- app/src/main/java/com/grupo20/finapps/fastrackt/LoginActivity.java
+++ app/src/main/java/com/grupo20/finapps/fastrackt/LoginActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Looper;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -11,13 +12,28 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.protocol.HTTP;
+
+import java.io.InputStream;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private static final String SERVER_URL = "http://192.168.10.45:6969/checkCredentials";
+
     private SharedPreferences prefs;
+    private Toast toast = null;
 
     @Bind(R.id.ownerName)
     EditText ownerName;
@@ -30,6 +46,9 @@ public class LoginActivity extends AppCompatActivity {
 
     @Bind(R.id.doneIdentify)
     ImageView doneIdentify;
+
+    String owner;
+    String number;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,45 +73,73 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public void setIdentifyInfo() {
-        String owner = ownerName.getText().toString();
+        owner = ownerName.getText().toString();
         String pin = cardPin.getText().toString();
-        String number = cardNumber.getText().toString();
+        number = cardNumber.getText().toString();
 
-        boolean valid = sendDataToServer(owner, number, pin);
-
-        if (valid) {
-            SharedPreferences.Editor editor = prefs.edit();
-
-            editor.putString("ownerName", owner);
-            editor.putString("number", number);
-            editor.apply();
-
-            startActivity(new Intent(LoginActivity.this, MapsActivity.class));
-        }
-        else {
-            // Mostrar error
-        }
+        sendDataToServer(owner, number, pin);
     }
 
-    private boolean sendDataToServer(String owner, String number, String pin) {
-        return true;
+    private void onDataCorrect() {
+        SharedPreferences.Editor editor = prefs.edit();
+
+        editor.putString("ownerName", owner);
+        editor.putString("number", number);
+        editor.apply();
+
+        startActivity(new Intent(LoginActivity.this, MapsActivity.class));
     }
 
-    private boolean isValid(String input, String valor) {
-        if (valor == null) return false;
+    private void mostraToast(String txt) {
+        if (toast != null) toast.cancel();
+        toast = Toast.makeText(getApplicationContext(),
+                txt,
+                Toast.LENGTH_SHORT);
+        toast.show();
+    }
 
-        switch(input) {
-            case "ownerName":
 
-            break;
-            case "pin":
+    protected void sendJson(final String json) {
+        Thread t = new Thread() {
 
-            break;
-            case "number":
+            public void run() {
+                Looper.prepare(); //For Preparing Message Pool for the child Thread
+                HttpClient client = new DefaultHttpClient();
+                HttpConnectionParams.setConnectionTimeout(client.getParams(), 10000); //Timeout Limit
+                HttpResponse response;
 
-            break;
-        }
+                try {
+                    HttpPost post = new HttpPost(SERVER_URL);
+                    StringEntity se = new StringEntity( json);
+                    se.setContentType(new BasicHeader(HTTP.CONTENT_TYPE, "application/json"));
+                    post.setEntity(se);
+                    response = client.execute(post);
 
-        return true;
+                    /*Checking response */
+                    if(response!=null){
+                        InputStream in = response.getEntity().getContent(); //Get the data in the entity
+                        String responseString = IOUtils.toString(in, "UTF-8");
+                        if (responseString.equals("OK")) {
+                            onDataCorrect();
+                        }
+                        else {
+                            mostraToast("Hi ha hagut un error del servidor en enviar les dades");
+                        }
+                    }
+
+                } catch(Exception e) {
+                    e.printStackTrace();
+                    mostraToast("No es pot establir la connexió");
+                }
+
+                Looper.loop(); //Loop in the message queue
+            }
+        };
+
+        t.start();
+    }
+
+    private void sendDataToServer(String owner, String number, String pin) {
+        sendJson("{ 'owner': " + owner + ", 'number': " + number + ", 'pin': " + pin + " }");
     }
 }
