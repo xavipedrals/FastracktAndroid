b0cc2421deab754d5e27db33217f1023a574ee0c
Xavier Pedrals <xavier.pedrals.camprubi@est.fib.upc.edu>
2015-11-07 05:42:52 +0100

    splash windows finished

--- app/src/main/java/com/grupo20/finapps/fastrackt/MainActivity.java
+++ app/src/main/java/com/grupo20/finapps/fastrackt/MainActivity.java
@@ -44,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    @Override
+/*    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
@@ -64,5 +64,5 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
+    }*/
 }
