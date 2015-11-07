743712371fc52488f39f188065dc523e915e3f4e
Albert Segarra Roca <albertsgrc@gmail.com>
2015-11-07 05:17:01 +0100

    Added input checking methods

--- app/src/main/java/com/grupo20/finapps/fastrackt/LoginActivity.java
+++ app/src/main/java/com/grupo20/finapps/fastrackt/LoginActivity.java
@@ -41,21 +41,44 @@ public class LoginActivity extends AppCompatActivity {
         doneIdentify.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                getIdentifyInfo(v);
+                getIdentifyInfo();
             }
         });
     }
 
-    public void getIdentifyInfo(ImageView imageView) {
+    public void getIdentifyInfo() {
         String owner = ownerName.getText().toString();
         String pin = cardPin.getText().toString();
         String number = cardNumber.getText().toString();
 
-        SharedPreferences.Editor editor = prefs.edit();
+        if (isValid("owner", owner) && isValid("pin", pin) && isValid("number", number)) {
+            SharedPreferences.Editor editor = prefs.edit();
 
-        editor.putString("ownerName", owner);
-        editor.putString("pin", pin);
-        editor.putString("number", number);
-        editor.apply();
+            editor.putString("ownerName", owner);
+            editor.putString("pin", pin);
+            editor.putString("number", number);
+            editor.apply();
+        }
+        else {
+            // Mostrar error
+        }
+    }
+
+    private boolean isValid(String input, String valor) {
+        if (valor == null) return false;
+
+        switch(input) {
+            case "ownerName":
+
+            break;
+            case "pin":
+
+            break;
+            case "number":
+
+            break;
+        }
+
+        return true;
     }
 }
