eb033f134bc295adbb2ed4fcaabb894efaa54c25
Xavier Pedrals <xavier.pedrals.camprubi@est.fib.upc.edu>
2015-11-07 04:51:18 +0100

    first image added

--- app/src/main/java/com/grupo20/finapps/fastrackt/AfterMain.java
+++ app/src/main/java/com/grupo20/finapps/fastrackt/AfterMain.java
@@ -5,17 +5,21 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.ImageView;
+import butterknife.Bind;
+import butterknife.OnClick;
 
-/**
- * Created by xavi on 07/11/15.
- */
-public class MainActivity extends AppCompatActivity {
+import java.util.Map;
 
-    private static int TIME_OUT = 2000;
+public class AfterMain extends AppCompatActivity {
 
     Button mapsButton;
 
@@ -24,16 +28,20 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_image);
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent i = new Intent(MainActivity.this, AfterMain.class);
-                startActivity(i);
-                finish();
-            }
-        }, TIME_OUT);
+        setContentView(R.layout.activity_main);
 
+        prefs = getSharedPreferences("user", Context.MODE_PRIVATE);
+
+        redirectActivity();
+    }
+
+    private void redirectActivity() {
+        if (prefs.getString("ownerName", "").equals("")) {
+            startActivity(new Intent(AfterMain.this, LanguageSelectionActivity.class));
+        }
+        else {
+            startActivity(new Intent(AfterMain.this, MapsActivity.class));
+        }
     }
 
     @Override
